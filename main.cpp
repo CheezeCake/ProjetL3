737d47c05c06e05d244c6d14b2d3aa1c9399238e
CheezeCake <emmanuel.nicolet@gmail.com>
2013-10-18 21:14:39 +0200

    faut virer ce main

--- main.cpp
+++ main.cpp
@@ -1,43 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 #include "solver.hpp"
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-	vector<string> villes(6);
-	vector<vector<double> > distances(6);
-	for(int i = 0; i < 6; i++)
+	if(argc == 1)
+		return 1;
+
+	int n = atoi(argv[1]);
+	vector<string> villes(n);
+	vector<vector<double> > distances(n);
+	for(int i = 0; i < n; i++)
 	{
-		distances[i].resize(6, 0);
+		distances[i].resize(n, 0);
 	}
 
-	villes[0] = "Marseille";
-	villes[1] = "Paris";
-	villes[2] = "Avignon";
-	villes[3] = "Lille";
-	villes[4] = "Lyon";
-	villes[5] = "Loriol";
-
-	distances[0][1] = 500;
-	distances[0][2] = 100;
-	distances[0][3] = 1000;
-	distances[0][4] = 300;
-	distances[0][5] = 150;
-
-	distances[1][0] = 500;
-	distances[1][2] = 400;
-	distances[1][3] = 200;
-	distances[1][4] = 300;
-	distances[1][5] = 450;
-	
-	distances[2][0] = 100;
-	distances[2][1] = 400;
-	distances[2][3] = 500;
-	distances[2][4] = 450;
-	distances[2][5] = 100;
-
-	Solver S(villes, distances, 5);
+	Solver S(villes, distances, n/2);
 	S.afficher();
 
 	return 0;
