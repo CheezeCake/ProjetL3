9d8811bf021911bd2928d3ade1e19147bc615217
CheezeCake <emmanuel.nicolet@gmail.com>
2013-10-20 03:36:58 +0200

    structure Secteur

--- solver.hpp
+++ solver.hpp
@@ -5,6 +5,13 @@
 #include <string>
 #include "solution.hpp"
 
+struct Secteur
+{
+	int bi;
+	int bs;
+	int indice;
+};
+
 class Solver
 {
 	private:
@@ -16,7 +23,7 @@ class Solver
 
 		void genererPI();
 		bool presente(const std::vector<int>&, int) const;
-		static int getSecteurId(const std::vector<int>&, int);
+		static int getSecteurId(const std::vector<Secteur>&, int);
 
 	public:
 		Solver(const std::vector<std::string>&, const std::vector<std::vector<double> >&, int);
