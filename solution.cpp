981de2727a12382f923c586dddfb935fc61fe87e
CheezeCake <emmanuel.nicolet@gmail.com>
2013-10-28 20:32:56 +0100

    slincingCrossove() + calculerScore

--- solution.cpp
+++ solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "rand.hpp"
 #include "solution.hpp"
 using namespace std;
 
@@ -10,18 +11,23 @@ Solution::Solution()
 Solution::Solution(const vector<int> &parcours, const vector<vector<double> > &distances)
 {
 	this->parcours = parcours;
-	score = 0;
-
-	int N = parcours.size();
-	for(int i = 0; i < N-1; i++)
-		score += distances[parcours[i]][parcours[i+1]];
-
-	score += distances[parcours[N-1]][parcours[0]];
+	calculerScore(distances);
 }
 
 double Solution::getScore() const
 {
 	return score;
+}
+
+void Solution::calculerScore(const std::vector<std::vector<double> > &distances)
+{
+	int N = parcours.size();
+
+	score = 0;
+	for(int i = 0; i < N-1; i++)
+		score += distances[parcours[i]][parcours[i+1]];
+
+	score += distances[parcours[N-1]][parcours[0]];
 }
 
 bool Solution::operator==(const Solution &s) const
@@ -68,4 +74,24 @@ bool Solution::operator==(const vector<int> &parcours) const
 bool Solution::operator<(const Solution &s) const
 {
 	return (score < s.score);
+}
+
+void Solution::slicingCrossover(const Solution &s, Solution &fils1, Solution &fils2) const
+{
+	int N = parcours.size();
+	fils1.parcours.resize(N);
+	fils2.parcours.resize(N);
+
+	int ptCoupure = Rand::randi(N-1)+1; //1 -> N-1
+	for(int i = 0; i < ptCoupure; i++)
+	{
+		fils1.parcours[i] = parcours[i];
+		fils2.parcours[i] = s.parcours[i];
+	}
+
+	for(int i = ptCoupure; i < N; i++)
+	{
+		fils1.parcours[i] = s.parcours[i];
+		fils2.parcours[i] = parcours[i];
+	}
 }
--- solution.hpp
+++ solution.hpp
@@ -13,10 +13,13 @@ class Solution
 		Solution();
 		void afficher() const;
 		double getScore() const;
+		void calculerScore(const std::vector<std::vector<double> >&);
 		Solution(const std::vector<int>&, const std::vector<std::vector<double> >&);
 		bool operator==(const Solution&) const;
 		bool operator==(const std::vector<int>&) const;
 		bool operator<(const Solution&) const;
+		void slicingCrossover(const Solution&, Solution&, Solution&) const;
+		void slicingCrossover(int, const Solution&, Solution&, Solution&) const;
 };
 
 #endif
